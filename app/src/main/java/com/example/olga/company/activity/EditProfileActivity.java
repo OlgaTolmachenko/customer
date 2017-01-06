35e90df52479cbef573e80f5cb6b619bf8494ad7
Olga <o.tolmachenko@mobidev.biz>
2017-01-06 12:51:05 +0200

    SignIn layout ready

---
+++ app/src/main/java/com/example/olga/company/activity/EditProfileActivity.java
@@ -0,0 +1,10 @@
+package com.example.olga.company.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by olga on 06.01.17.
+ */
+
+public class EditProfileActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/olga/company/activity/SignInActivity.java
@@ -0,0 +1,32 @@
+package com.example.olga.company.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.olga.company.R;
+
+/**
+ * Created by olga on 06.01.17.
+ */
+
+public class SignInActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
